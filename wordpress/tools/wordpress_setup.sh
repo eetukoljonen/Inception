1a1773990ef7d8f558bfe6e6726e3abe7127253b
eetu koljonen <eetu.koljonen@gmail.com>
2024-03-07 15:22:00 +0200

    okaaaaaay let's go

--- wordpress/tools/wordpress_setup.sh
+++ wordpress/tools/wordpress_setup.sh
@@ -2,7 +2,7 @@
 
 # Wait for MariaDB to be ready
 attempts=0
-while ! mariadb -h$MYSQL_HOST -u$WORDPRESS_USER -p$WORDPRESS_PW $WORDPRESS_DATABASE &>/dev/null; do
+while ! mariadb -h$MYSQL_HOST -u$WORDPRESS_USER -p$WORDPRESS_PW $WORDPRESS_DATABASE; do
 	attempts=$((attempts + 1))
     echo "MariaDB unavailable. Attempt $attempts: Trying again in 5 sec."
 	if [ $attempts -ge 12 ]; then
@@ -30,14 +30,20 @@ chmod +x /usr/local/bin/wp
 # DL WP using the CLI
 wp core download --allow-root
 
+wp --info
+
+echo "1"
+
 # Create WordPress database config
-wp config create \
+wp config create --allow-root \
 	--dbname=$WORDPRESS_DATABASE \
 	--dbuser=$WORDPRESS_USER \
 	--dbpass=$WORDPRESS_PW \
 	--dbhost=$MYSQL_HOST \
 	--path=/var/www/html/wordpress/ \
 	--force
+
+echo "2"
 
 # Install WordPress and feed db config
 wp core install \
@@ -51,6 +57,9 @@ wp core install \
 	--path=/var/www/html/wordpress/
 
 # Create WordPress user
+
+echo "3"
+
 wp user create \
 	$WORDPRESS_USER \
 	$WORDPRESS_EMAIL \
@@ -58,17 +67,24 @@ wp user create \
 	--user_pass=$WORDPRESS_PW \
 	--allow-root
 
+echo "3"
+
 # Install theme for WordPress
 wp theme install inspiro \
 	--activate \
 	--allow-root
 
+echo "4"
 # Update plugins
-wp plugin update --all
+wp plugin update --all --allow-root
+
+echo "5"
 
 # Update WP address and site address to match our domain
 wp option update siteurl "https://$DOMAIN" --allow-root
 wp option update home "https://$DOMAIN" --allow-root
+
+echo "6"
 
 # Transfer ownership to the user
 chown -R nginx:nginx /var/www/html/wordpress
@@ -77,4 +93,4 @@ chown -R nginx:nginx /var/www/html/wordpress
 chmod -R 755 /var/www/html/wordpress
 
 # Fire up PHP-FPM (-F to keep in foreground and avoid recalling script)
-php-fpm81 -F
+php-fpm7.4 -F
